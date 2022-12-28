08119b94347ce6b8bbe955d5f55aa3d25e0d1872
apinanyogaratnam <apinanapinan@icloud.com>
2022-12-27 20:39:00 -0500

    feat: created rank_wealth function

--- crypto_wealth_rank/main.py
+++ crypto_wealth_rank/main.py
@@ -1,2 +1,20 @@
-def greetings():
-    return "Hello, World!"
+def rank_wealth(balance: float | int | None) -> str | None:
+    if balance is None:
+        return None
+
+    wealth_ranks = {
+        '1': 'Shrimp',
+        '10': 'Crab',
+        '50': 'Octopus',
+        '100': 'Fish',
+        '500': 'Dolphin',
+        '1000': 'Shark',
+        '5000': 'Whale',
+    }
+
+    try:
+        balance = str(float(balance))
+    except ValueError as e:
+        raise ValueError('balance must be an integer or a float') from e
+
+    return wealth_ranks.get(balance, 'HumpBack Whale')
